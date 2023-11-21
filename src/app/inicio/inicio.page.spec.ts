3e53ae0bf583dd2338436fa323829ccb37f84fb7
josue <thenashrem@gmail.com>
2023-11-21 11:42:32 -0300

    integracion de camara

--- src/app/inicio/inicio.page.spec.ts
+++ src/app/inicio/inicio.page.spec.ts
@@ -1,15 +1,21 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { InicioPage } from './inicio.page';
+import { IonicModule } from '@ionic/angular';
 
 describe('InicioPage', () => {
   let component: InicioPage;
   let fixture: ComponentFixture<InicioPage>;
 
-  beforeEach(async(() => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [InicioPage],
+      imports: [IonicModule.forRoot()]
+    }).compileComponents();
+
     fixture = TestBed.createComponent(InicioPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  }));
+  });
 
   it('should create', () => {
     expect(component).toBeTruthy();
